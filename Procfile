69bc2297fca855b22cc23aed7659be4a7bab7113
amrit-srivastava <amrit.srivastava@gmail.com>
2021-10-30 12:27:13 +0530

    heroku-changes

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: contactsapp.wsgi
--- contactapp/settings.py
+++ contactapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "riu-aa*3cdls)e4vfz6mc4h^_74q@85nv4hkmn@f$=&%095*=0"
+SECRET_KEY = os.environ.get("CONTACTSAPP_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+
+django_heroku.settings(locals())
